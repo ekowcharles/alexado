312f7a672a30a1065523907812ecb7efd88d8c6c
Charles Boadu <ekowcharles@gmail.com>
2019-03-03 04:15:26 -0600

    Omit fields for attributes when empty

--- request.go
+++ request.go
@@ -39,8 +39,8 @@ type System struct {
 }
 
 type Attributes struct {
-	Key   string `json:"key"`
-	Value string `json:"value"`
+	Key   string `json:"key,omitempty"`
+	Value string `json:"value,omitempty"`
 }
 
 type Device struct {
