e48705bb94f8f76e498ca767b4f4a298b46d613e
Charles Boadu <ekowcharles@gmail.com>
2019-03-11 18:58:49 -0500

    Add more fields for alexa respone

--- response.go
+++ response.go
@@ -3,63 +3,117 @@ package alexado
 
 import "encoding/json"
 
+// AlexaResponse is the response to be sent to the Alexa platform
 type AlexaResponse struct {
-	Version           string      `json:"version,omitempty"`
-	SessionAttributes *Attributes `json:"sessionAttributes,omitempty"`
-	Response          Response    `json:"response,omitempty"`
+	Version           string      `json:"version,omitempty"`           // version specifier for the response with the value to be defined as: "1.0"
+	SessionAttributes *Attributes `json:"sessionAttributes,omitempty"` // map of key-value pairs to persist in the session
+	Response          Response    `json:"response,omitempty"`          // defines what to render to the user and whether to end the current session
 }
 
-func (t AlexaResponse) ToJSON() (string, error) {
-	toJSON, err := json.Marshal(t)
-
-	return string(toJSON), err
-}
-
+// Response defines what to render to the user and whether to end the current session
 type Response struct {
-	OutputSpeech     *OutputSpeech `json:"outputSpeech,omitempty"`
-	Card             *Card         `json:"card,omitempty"`
-	Reprompt         *Reprompt     `json:"reprompt,omitempty"`
-	Directives       []Directives  `json:"directives,omitempty"`
-	ShouldEndSession bool          `json:"shouldEndSession,omitempty"`
+	OutputSpeech     *OutputSpeech `json:"outputSpeech,omitempty"`     // contains the type of output speech to render
+	Card             *Card         `json:"card,omitempty"`             // contains a card to render to the Amazon Alexa App
+	Reprompt         *Reprompt     `json:"reprompt,omitempty"`         // contains the outputSpeech to use if a re-prompt is necessary
+	Directives       []Directive   `json:"directives,omitempty"`       // specifies device-level actions to take using a particular interface, such as the AudioPlayer interface for streaming audio
+	ShouldEndSession bool          `json:"shouldEndSession,omitempty"` // true meaning that the session should end after Alexa speaks the response, or false if the session should remain active. If not provided, defaults to true.
 }
 
+// OutputSpeech is used for setting both the outputSpeech and the reprompt properties
 type OutputSpeech struct {
-	Type         string `json:"type,omitempty"`
-	Text         string `json:"text,omitempty"`
-	SSML         string `json:"ssml,omitempty"`
-	PlayBehavior string `json:"playBehavior,omitempty"`
+	Type         string `json:"type,omitempty"`         // contains the type of output speech to render
+	Text         string `json:"text,omitempty"`         // contains the speech to render to the user
+	SSML         string `json:"ssml,omitempty"`         // containing text marked up with SSML to render to the user. Use this when type is  "SSML"
+	PlayBehavior string `json:"playBehavior,omitempty"` // determines the queuing and playback of this output speech
 }
 
+// OutputSpeechType is the type of output speech to render
 type OutputSpeechType int
 
 const (
-	SSML OutputSpeechType = 0
+	// SSML indicates that the output speech is text marked up with SSML
+	SSML OutputSpeechType = iota
+	// PlainText indicates that the output speech is defined as plain text
+	PlainText
 )
 
 func (o OutputSpeechType) String() string {
-	types := [...]string{
+	return [...]string{
 		"SSML",
-	}
-
-	return types[o]
+		"PlainText",
+	}[o]
 }
 
+// PlayBehaviorType determines the queuing and playback of this output speech
+type PlayBehaviorType int
+
+const (
+	// Enqueue adds this speech to the end of the queue. Do not interrupt Alexa's current speech. This is the default value for all skills that do not use the GameEngine interface
+	Enqueue PlayBehaviorType = iota
+	// ReplaceAll immediately begins playback of this speech, and replace any current and enqueued speech. This is the default value for all skills that use the GameEngine interface
+	ReplaceAll
+	// ReplaceEnqueued replaces all speech in the queue with this speech. Do not interrupt Alexa's current speech
+	ReplaceEnqueued
+)
+
+func (p PlayBehaviorType) String() string {
+	return [...]string{
+		"ENQUEUE",
+		"REPLACE_ALL",
+		"REPLACE_ENQUEUED",
+	}[p]
+}
+
+// Card can only be included when sending a response to a CanFulfillIntentRequest, LaunchRequest, IntentRequest, or InputHandlerEvent
 type Card struct {
-	Type  string `json:"type,omitempty"`
-	Title string `json:"title,omitempty"`
-	Text  string `json:"text,omitempty"`
-	Image *Image `json:"image,omitempty"`
+	Type  string `json:"type,omitempty"`  // describes the type of card to render
+	Title string `json:"title,omitempty"` // contains the title of the card. (not applicable for cards of type LinkAccount).
+	Text  string `json:"text,omitempty"`  // contains the text content for a Standard card (not applicable for cards of type Simple or LinkAccount)
+	Image *Image `json:"image,omitempty"` // specifies the URLs for the image to display on a Standard card. Only applicable for Standard cards.
 }
 
+// CardType describes the type of card to render
+type CardType int
+
+const (
+	// Simple for when card contains a title and plain text content
+	Simple CardType = iota
+	// Standard for when card contains a title, text content, and an image to display
+	Standard
+	// LinkAccount for when card displays a link to an authorization URI that the user can use to link their Alexa account with a user in another system
+	LinkAccount
+	// AskForPermissionsConsent for when card asks the customer for consent to obtain specific customer information, such as Alexa lists or address information
+	AskForPermissionsConsent
+)
+
+func (c CardType) String() string {
+	return [...]string{
+		"Simple",
+		"Standard",
+		"LinkAccount",
+		"AskForPermissionsConsent",
+	}[c]
+}
+
+// Image specifies the URLs for the image to display on a Standard card. Only applicable for Standard cards.
 type Image struct {
-	SmallImageURL string `json:"smallImageUrl,omitempty"`
-	LargeImageURL string `json:"largeImageUrl,omitempty"`
+	SmallImageURL string `json:"smallImageUrl,omitempty"` // displayed on smaller screens
+	LargeImageURL string `json:"largeImageUrl,omitempty"` // displayed on larger screens
 }
 
+// Reprompt contains the outputSpeech to use if a re-prompt is necessary
 type Reprompt struct {
 	OutputSpeech OutputSpeech `json:"outputSpeech,omitempty"`
 }
 
-type Directives struct {
+// Directive specifies device-level actions to take using a particular interface, such as the AudioPlayer interface for streaming audio
+type Directive struct {
 	Type string `json:"type,omitempty"`
+}
+
+// ToJSON converts the AlexaResponse object to json format
+func (t AlexaResponse) ToJSON() (string, error) {
+	toJSON, err := json.Marshal(t)
+
+	return string(toJSON), err
 }
--- response_test.go
+++ response_test.go
@@ -2,6 +2,63 @@ package alexado
 
 import "testing"
 
+func TestOutputSpeechTypeString(t *testing.T) {
+	var actual, expected string
+
+	actual, expected = SSML.String(), "SSML"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = PlainText.String(), "PlainText"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+}
+
+func TestPlayBehaviorTypeString(t *testing.T) {
+	var actual, expected string
+
+	actual, expected = Enqueue.String(), "ENQUEUE"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = ReplaceAll.String(), "REPLACE_ALL"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = ReplaceEnqueued.String(), "REPLACE_ENQUEUED"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+}
+
+func TestCardTypeString(t *testing.T) {
+	var actual, expected string
+
+	actual, expected = Simple.String(), "Simple"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = Standard.String(), "Standard"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = LinkAccount.String(), "LinkAccount"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+
+	actual, expected = AskForPermissionsConsent.String(), "AskForPermissionsConsent"
+	if actual != expected {
+		t.Errorf("'%s' != '%s'", actual, expected)
+	}
+}
+
 func TestToJSON(t *testing.T) {
 	sa := Attributes{Key: "foo", Value: "bar"}
 	osp := OutputSpeech{Type: SSML.String(), SSML: "Some good speech."}
